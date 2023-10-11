5532cf2ab88bd267bcb5f13fb1b74a57ae6783ab
Ro706 <rohitmandal99091@gmail.com>
2023-10-12 00:54:46 +0530

    add file

---
+++ pattern/right_angle_triangle_right_side.c
@@ -0,0 +1,16 @@
+//right angle triangle right side
+#include<stdio.h>
+int main(){
+	int row = 5,col = 5;
+	for (int row =0;row <= 5;row++){
+		for (int col = 0;col<(5-row);col++){
+			printf(" ");
+		}
+		for(int k = 5-row;k<5;k++){
+		 	printf("* ");
+		}
+		
+		printf("\n");
+	}
+	return 0;
+}
