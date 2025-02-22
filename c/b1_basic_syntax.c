4af150cfdffd84ded33cf927caaf7b6b95d56dc8
Ro706 <rohitmandal99091@gmail.com>
2025-02-22 21:25:59 +0530

    update add basic syntax of c and c++

---
+++ c/b1_basic_syntax.c
@@ -0,0 +1,20 @@
+/*
+ This code is Written for basic under standing of C syntax
+ how it is process and and how it compiled 
+
+ This code is written by Ro706
+
+
+ Basic information about c language
+ c is middle-level programming language refferred as the "mother of all prrogramming language".
+ type -> compiled 
+ paradigram-> proceddural ,structure
+ Memory -> manual
+ syntax -> complex
+ */
+
+#include<stdio.h> // this line import statement...basically it importing the stander library of input out in c language 
+int main(){ // this is a main function without this code will not run because it give the flow of the code
+	printf("Hello world"); //this is a printing statment ..right now it is printing hello world line
+	return 0; //as our function type is int so we ensure it return some value like 0 , 1, 2 ..etc
+}
